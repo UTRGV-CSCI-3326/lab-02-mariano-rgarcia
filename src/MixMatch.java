589c6e810881ecfd042fdcb1bb8d92014200785d
mariano-rgarcia <mariano.garcia04@utrgv.edu>
2025-02-02 02:50:33 +0000

    Final lab2

---
+++ src/MixMatch.java
@@ -0,0 +1,30 @@
+public class MixMatch {
+    public static void main(String [] args){
+        // declare and initialize variables
+        String str = "Hello!"; //string
+
+        char ch = 'A': // char
+
+        short sh = 32000; // short
+
+        long lng = 10000000000L; // long
+
+        int num = 1; // int
+
+        float flt = 5.75f; // float
+
+        double dbl = 1.21; // double
+
+        boolean bool = true; // boolean
+
+        // Print the variables
+        System.out.println("String: " + str);
+        System.out.println("Char: " + ch);
+        System.out.println("Short: " + sh);
+        System.out.println("Int: " + num);
+        System.out.println("Long: " + lng);
+        System.out.println("Float: " + flt);
+        System.out.println("Double: " + dbl);
+        System.out.println("Boolean: " + bool);
+    }
+}
